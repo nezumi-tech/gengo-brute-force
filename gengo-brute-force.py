6d384d07d17da754606708a4824953e7ee7fb603
nezumi_tech <nezumi_tech@yahoo.co.jp>
2019-02-28 20:34:13 +0900

    Update gengo-brute-force.py

--- gengo-brute-force.py
+++ gengo-brute-force.py
@@ -2,21 +2,15 @@
 """
 Created on Thu Feb 28 18:29:20 2019
 
-@author: nezum
+@author: nezumi_tech
 """
 
 import pandas as pd
-
 joyo = pd.read_csv("joyo_kanji.csv", header=None)
     
 joyo_list = joyo.values.tolist()
-    
-gengo_list = [] 
 
-for a in range(len(joyo_list) - 1):
-    for b in range(len(joyo_list) - 1):
-        gengo_list.append(''.join(joyo_list[a] + joyo_list[b]))
-
-gengo = pd.DataFrame(gengo_list)
-
-gengo.to_csv("gengo.csv", header=False, index=False)
+with open('gengo.csv', mode='w', encoding='utf-8') as f:
+    for a in range(len(joyo_list) - 1):
+        for b in range(len(joyo_list) - 1):
+            f.write(''.join(joyo_list[a] + joyo_list[b])+'\n')
